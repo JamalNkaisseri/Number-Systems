f560c9a27749e9701de0a0c5bbbdfaa43ee505fa
JS <jackdesparrow660@gmail.com>
2023-07-11 16:01:40 +0300

    Add combo box

--- src/main/java/com/example/numbersystems/View.java
+++ src/main/java/com/example/numbersystems/View.java
@@ -1,8 +1,12 @@
 package com.example.numbersystems;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -14,6 +18,9 @@ import java.io.IOException;
 public class View extends Application {
 
     TextField inputTextField;
+    TextField outputTextField;
+
+    Button button;
 
     Label decimalLabel,
           binaryLabel,
@@ -24,16 +31,29 @@ public class View extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10,10,10,10));
+        grid.setVgap(10);
+        grid.setHgap(10);
+
         inputTextField = new TextField();
         inputTextField.setPromptText("Enter a number");
-        inputTextField.setPrefWidth(5);
-        inputTextField.setStyle("-fx-prompt-text-fill: #FF0000;");
+        inputTextField.setPrefWidth(150);
+        inputTextField.setAlignment(Pos.CENTER_LEFT);
+        inputTextField.setStyle("-fx-prompt-text-fill: #000000;");
 
-        VBox layout = new VBox(10);
-        layout.setPadding(new Insets(10));
-        layout.getChildren().addAll(inputTextField);
+        ComboBox <String> comboBox = new ComboBox<>();
+        comboBox.setItems(FXCollections.observableArrayList("Decimal","Binary","Hexadecimal","Octal"));
+        comboBox.setPromptText("Select an option");
+        GridPane.setConstraints(comboBox,0,1);
 
-        Scene scene = new Scene(layout, 300, 300);
+
+        button = new Button("Convert");
+        GridPane.setConstraints(button,0,2);
+
+        grid.getChildren().addAll(inputTextField,button,comboBox);
+
+        Scene scene = new Scene(grid, 300, 300);
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
