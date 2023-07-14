96b0e1b85f766e31a795ec2b6f06303afa39e07d
JS <jackdesparrow660@gmail.com>
2023-07-14 14:06:28 +0300

    Improve UI

--- src/main/java/com/example/numbersystems/View.java
+++ src/main/java/com/example/numbersystems/View.java
@@ -1,7 +1,6 @@
 package com.example.numbersystems;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -16,45 +15,103 @@ import java.io.IOException;
 
 public class View extends Application {
 
-    ComboBox<String> conversionComboBox;
+    ComboBox<String> decimalConversionComboBox;
+    ComboBox<String> binaryConversionComboBox;
+    ComboBox<String> octalConversionComboBox;
+    ComboBox<String> hexadecimalConversionComboBox;
 
     TextField decimalTextField;
     TextField binaryTextField;
     TextField octalTextField;
     TextField hexadecimalTextField;
-    TextField convertedTextField;
+    TextField decimalConvertedTextField;
+    TextField binaryConvertedTextField;
+    TextField octalConvertedTextField;
+    TextField hexadecimalConvertedTextField;
 
-    Button decimalToBinaryButton;
-    Button decimalToOctalButton;
-    Button decimalToHexadecimalButton;
-    Button convertButton;
+
+
+    Button decimalConvertButton;
+    Button binaryConvertButton;
+    Button octalConvertButton;
+    Button hexadecimalConvertButton;
 
     Label decimalLabel,
           binaryLabel,
           hexadecimalLabel,
+          hexadecimalConvertedLabel,
+          hexadecimalConversionLabel,
           octalLabel,
-          conversionLabel,
-          convertedLabel
+          octalConvertedLabel,
+          octalConversionLabel,
+          decimalConversionLabel,
+          decimalConvertedLabel,
+          binaryConversionLabel,
+          binaryConvertedLabel
+
     ;
 
     @Override
     public void start(Stage stage) throws IOException {
 
 
-        // Create UI elements
+        // Create decimal UI elements
         decimalLabel = new Label("Decimal");
         decimalTextField = new TextField();
-        decimalToBinaryButton = new Button("Decimal to Binary");
+       // decimalToBinaryButton = new Button("Decimal to Binary");
 
-        conversionLabel = new Label("Conversion");
-        conversionComboBox = new ComboBox<>();
-        conversionComboBox.getItems().addAll("Binary","Octal","Hexadecimal");
+        decimalConversionLabel = new Label("Conversion");
+        decimalConversionComboBox = new ComboBox<>();
+        decimalConversionComboBox.getItems().addAll("Binary","Octal","Hexadecimal");
 
-        convertButton = new Button("Convert");
+        decimalConvertButton = new Button("Convert");
 
-        convertedLabel = new Label("Converted");
-        convertedTextField = new TextField();
-        convertedTextField.setEditable(false);
+        decimalConvertedLabel = new Label("Converted");
+        decimalConvertedTextField = new TextField();
+        decimalConvertedTextField.setEditable(false);
+
+        // Create binary UI elements
+        binaryLabel = new Label("Binary");
+        binaryTextField = new TextField();
+
+        binaryConversionLabel =  new Label("Conversion");
+        binaryConversionComboBox = new ComboBox<>();
+        binaryConversionComboBox.getItems().addAll("Decimal","Octal","Hexadecimal");
+
+        binaryConvertButton = new Button("Convert");
+
+        binaryConvertedLabel =  new Label("Converted");
+        binaryConvertedTextField = new TextField();
+        binaryConvertedTextField.setEditable(false);
+
+        // Create octal UI elements
+        octalLabel = new Label("Octal");
+        octalTextField = new TextField();
+
+        octalConversionLabel = new Label("Conversion");
+        octalConversionComboBox = new ComboBox<>();
+        octalConversionComboBox.getItems().addAll("Decimal","Binary","Hexadecimal");
+
+        octalConvertButton = new Button("Convert");
+
+        octalConvertedLabel = new Label("Converted");
+        octalConvertedTextField = new TextField();
+        octalConvertedTextField.setEditable(false);
+
+        // Create hexadecimal UI elements
+        hexadecimalLabel = new Label("Hexadecimal");
+        hexadecimalTextField = new TextField();
+
+        hexadecimalConversionLabel = new Label("Conversion");
+        hexadecimalConversionComboBox = new ComboBox<>();
+        hexadecimalConversionComboBox.getItems().addAll("Decimal","Binary","Hexadecimal");
+
+        hexadecimalConvertButton = new Button("Convert");
+
+        hexadecimalConvertedLabel = new Label("Converted");
+        hexadecimalConvertedTextField = new TextField();
+        hexadecimalConvertedTextField.setEditable(false);
+
 
         // Create layout
         GridPane grid = new GridPane();
@@ -63,17 +120,44 @@ public class View extends Application {
         grid.setVgap(10);
         grid.setAlignment(Pos.CENTER);
 
-        // Add elements to the layout
+        // Add decimal elements to the layout
         grid.add(decimalLabel, 0, 0);
         grid.add(decimalTextField, 1, 0);
-        grid.add(conversionLabel, 0, 1);
-        grid.add(conversionComboBox, 1, 1);
-        grid.add(convertButton, 2, 1);
-        grid.add(convertedLabel, 0, 2);
-        grid.add(convertedTextField, 1, 2);
+        grid.add(decimalConversionLabel, 0, 1);
+        grid.add(decimalConversionComboBox, 1, 1);
+        grid.add(decimalConvertButton, 2, 1);
+        grid.add(decimalConvertedLabel, 0, 2);
+        grid.add(decimalConvertedTextField, 1, 2);
+
+        // Add binary elements to the layout
+        grid.add(binaryLabel,0,4);
+        grid.add(binaryTextField,1,4);
+        grid.add(binaryConversionLabel,0,5);
+        grid.add(binaryConversionComboBox,1,5);
+        grid.add(binaryConvertButton,2,5);
+        grid.add(binaryConvertedLabel,0,6);
+        grid.add(binaryConvertedTextField,1,6);
+
+        // Add octal elements to the layout
+        grid.add(octalLabel,0,8);
+        grid.add(octalTextField,1,8);
+        grid.add(octalConversionLabel,0,9);
+        grid.add(octalConversionComboBox,1,9);
+        grid.add(octalConvertButton,2,9);
+        grid.add(octalConvertedLabel,0,10);
+        grid.add(octalConvertedTextField,1,10);
+
+        // Add hexadecimal elements to the layout
+        grid.add(hexadecimalLabel,0,12);
+        grid.add(hexadecimalTextField,1,12);
+        grid.add(hexadecimalConversionLabel,0,13);
+        grid.add(hexadecimalConversionComboBox,1,13);
+        grid.add(hexadecimalConvertButton,2,13);
+        grid.add(hexadecimalConvertedLabel,0,14);
+        grid.add(hexadecimalConvertedTextField,1,14);
 
 
-        Scene scene = new Scene(grid, 300, 300);
+        Scene scene = new Scene(grid, 400, 200);
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
